54d93ddc58765762d20d01a477fc803d09929f25
PagePetty <pagevpetty@live.com>
2019-05-25 23:53:43 -0600

    6. add pantry and shopping list icons and fragments

--- app/src/main/java/com/pagepetty/edible/PantryFragment.kt
+++ app/src/main/java/com/pagepetty/edible/PantryFragment.kt
@@ -1,11 +1,10 @@
-package com.pagepetty.notesnav
+package com.pagepetty.edible
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_main.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,20 +15,12 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class MainFragment : Fragment() {
-
+class PantryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        ibPantry.setOnClickListener {
-        }
+        return inflater.inflate(R.layout.fragment_pantry, container, false)
     }
 }
